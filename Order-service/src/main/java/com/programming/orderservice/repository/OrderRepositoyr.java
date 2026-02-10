b8f3bf67b57ccdccb14b08d3965abc480a34ae92
Huy Duy <duyhi1654@gmail.com>
2026-02-10 22:15:28 +0700

    remove

--- Order-service/src/main/java/com/programming/orderservice/repository/OrderRepositoyr.java
+++
@@ -1,5 +0,0 @@
-import com.programmingtechie.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
